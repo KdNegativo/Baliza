84b6244d18ea8bbfdcac280ad5fec7581a32d0d6
KdNegativo <antonioeduardosaraiva31@gmail.com>
2026-05-10 21:18:00 -0300

    Stabilize Samsung browser visuals

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import App from './App.jsx'
 
+if (/SamsungBrowser/i.test(window.navigator.userAgent)) {
+  document.documentElement.classList.add('samsung-browser')
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
